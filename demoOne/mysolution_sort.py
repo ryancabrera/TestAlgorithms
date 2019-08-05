7fe3e826a447125ad6d42c90ea2ccf6ea2c6922c
Ryan Cabrera <rcabrera@voceracommunications.com>
2019-08-05 14:45:27 -0700

    Testing out my ability to remember how to do this through an IDE

--- demoOne/mysolution_sort.py
+++ demoOne/mysolution_sort.py
@@ -1,6 +1,12 @@
 __Author__ = 'Ryan Cabrera'
 
+
 def sort(some_list):
+    for index in range(0, len(some_list)-1):
+        if some_list[index] > some_list[index+1]:
+            some_list[index], some_list[index+1] = some_list[index+1], some_list[index]
+            sort(some_list)
+
     return some_list
 
 
@@ -8,3 +14,7 @@ def main():
     nums = [5, 4, 6, 123, 4, 0, -1]
     for n in sort(nums):
         print(n)
+
+
+if __Author__:
+    main()
